c356915ce714dd4d86e768fc67a98c5af925f19d
severinsimmler <severin.simmler@posteo.de>
2019-10-25 00:50:31 +0200

    feat: corpus

---
+++ extract/corpus/__init__.py
@@ -0,0 +1,1 @@
+from extract.corpus.api import Corpus
---
+++ extract/corpus/api.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+from extract import utils
+
+
+class Corpus:
+    def __init__(self, directory: Path):
+        self.directory = directory.resolve()
+
+    def documents(self):
+        for document in self.directory.glob("*.txt"):
+            yield document.read_text(encoding="utf-8")
+
+    def tokens(self, **kwargs):
+        for document in self.documents:
+            yield utils.tokenize(document, **kwargs)
+
+    def sentences(self):
+        for document in self.documents:
+            yield utils.sentencize(document)
