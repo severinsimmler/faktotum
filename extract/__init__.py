14f079a1cb7ad503645977676710a52e7db69152
severinsimmler <severin.simmler@posteo.de>
2019-10-25 00:22:11 +0200

    feat: package

---
+++ extract/__init__.py
@@ -0,0 +1,1 @@
+from extract.utils import tokenize, sentencize
---
+++ extract/utils.py
@@ -0,0 +1,59 @@
+from typing import Generator
+
+import regex as re
+from spacy.lang.de import German
+
+
+LANGUAGE = German()
+TOKEN_PATTERN = re.compile(r"\p{L}+\p{P}?\p{L}+")
+
+
+def tokenize(
+    text: str, lowercase: bool = False, only_words: bool = False
+) -> Generator[str, None, None]:
+    """Split text into tokens.
+
+    Parameters
+    ----------
+    text
+        The text to split into tokens.
+    lowercase
+        If True, all tokens will be lowercase. Defaults to False.
+    only_words
+        If True, numbers and punctuation will be ignored. Defaults to False.
+
+    Yields
+    ------
+    One token at a time.
+    """
+    if only_words:
+        for token in TOKEN_PATTERN.finditer(text):
+            if lowercase:
+                yield token.group(0).lower()
+            else:
+                yield token.group(0)
+    else:
+        for token in LANGUAGE(text):
+            if lowercase:
+                yield token.text.lower()
+            else:
+                yield token.text
+
+
+def sentencize(text: str) -> Generator[str, None, None]:
+    """Split text into sentences.
+
+    Parameters
+    ----------
+    text
+        The text to split into tokens.
+
+    Yields
+    ------
+    One sentence at a time.
+    """
+    if "sentencizer" not in LANGUAGE.pipe_names:
+        sentencizer = LANGUAGE.create_pipe("sentencizer")
+        LANGUAGE.add_pipe(sentencizer)
+    for sentence in LANGUAGE(text).sents:
+        yield sentence
