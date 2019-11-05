5a83a39ddcf72f5628347ea58b6c1aac472e7520
severinsimmler <severin.simmler@posteo.de>
2019-11-05 18:00:00 +0100

    fix: typo

--- train.sh
+++ train.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 echo "#### Running TF-IDF..."
-poetry run tfidf --corpus /mnt/data/users/simmler/corpora/plaintext/gutenberg
-poetry run tfidf --corpus /mnt/data/users/simmler/corpora/plaintext/pressemitteilungen
+poetry run tfidf -- --corpus /mnt/data/users/simmler/corpora/plaintext/gutenberg
+poetry run tfidf -- --corpus /mnt/data/users/simmler/corpora/plaintext/pressemitteilungen
 
 echo "#### Running word2vec..."
-poetry run word2vec --corpus /mnt/data/users/simmler/corpora/plaintext/gutenberg --epochs 100 --algorithm cbow
-poetry run word2vec --corpus /mnt/data/users/simmler/corpora/plaintext/gutenberg --epochs 100 --algorithm skipgram
+poetry run word2vec -- --corpus /mnt/data/users/simmler/corpora/plaintext/gutenberg --epochs 100 --algorithm cbow
+poetry run word2vec -- --corpus /mnt/data/users/simmler/corpora/plaintext/gutenberg --epochs 100 --algorithm skipgram
 
-poetry run word2vec --corpus /mnt/data/users/simmler/corpora/plaintext/pressemitteilungen --epochs 100 --algorithm cbow
-poetry run word2vec --corpus /mnt/data/users/simmler/corpora/plaintext/pressemitteilungen --epochs 100 --algorithm skipgram
+poetry run word2vec  -- --corpus /mnt/data/users/simmler/corpora/plaintext/pressemitteilungen --epochs 100 --algorithm cbow
+poetry run word2vec -- --corpus /mnt/data/users/simmler/corpora/plaintext/pressemitteilungen --epochs 100 --algorithm skipgram
 
 echo "#### Running fastText..."
-poetry run fasttext --corpus /mnt/data/users/simmler/corpora/plaintext/gutenberg --epochs 100 --algorithm skipgram
+poetry run fasttext -- --corpus /mnt/data/users/simmler/corpora/plaintext/gutenberg --epochs 100 --algorithm skipgram
 
-poetry run fasttext --corpus /mnt/data/users/simmler/corpora/plaintext/pressemitteilungen --epochs 100 --algorithm skipgram
+poetry run fasttext -- --corpus /mnt/data/users/simmler/corpora/plaintext/pressemitteilungen --epochs 100 --algorithm skipgram
