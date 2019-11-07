c4456dcfd221bea7faf95380f66e9e89322d1df0
severinsimmler <severin.simmler@posteo.de>
2019-11-07 19:31:16 +0100

    fix: typo

--- extract/exploration/evaluation.py
+++ extract/exploration/evaluation.py
@@ -1,3 +1,4 @@
+import math
 import random
 
 from extract.corpus.core import Corpus
@@ -8,7 +9,7 @@ random.seed(23)
 
 
 def evaluate_topic_model(corpus, model):
-    correct = 0
+    scores = list()
     random_documents = select_random_documents(corpus)
     for i, document in enumerate(random_documents):
         print(f"Document {i + 1}/10: {document.name}")
@@ -17,13 +18,20 @@ def evaluate_topic_model(corpus, model):
         print("\n--------------------------------------------------\n")
         topics, intruder = select_topics(model, document.name)
         random.shuffle(topics)
-        for i, topic in enumerate(topics):
+        for i, (index, _) in enumerate(topics):
+            topic = ", ".join(model.topics[index])
             print(f"Topic {i}:", topic)
-        potential_intruder = input("\nWhich topic is the intruder? ")
+        guess = input("\nWhich topic is the intruder? ")
         print("\n##################################################\n\n\n")
-        if int(potential_intruder) == topics.index(intruder):
-            correct += 1
-    return correct / 10
+        guess = topics[int(guess)][1]
+        score = calculate_topic_log_odds(intruder, guess)
+        scores.append(score)
+    print(scores)
+    return scores
+
+
+def calculate_topic_log_odds(intruder: float, guess: float):
+    return math.log(intruder / guess)
 
 
 def select_random_documents(corpus: Corpus, n: int = 10):
@@ -33,10 +41,11 @@ def select_random_documents(corpus: Corpus, n: int = 10):
 
 def select_topics(model: TopicModel, document_name: str):
     distribution = model.document_topics.loc[:, document_name]
-    top3 = list(distribution.sort_values(ascending=False)[:3].index)
-    bottom3 = list(distribution.sort_values(ascending=False)[-3:].index)
-    intruder_index = random.choice(bottom3)
-    intruder = ", ".join(model.topics.iloc[intruder_index][:10])
-    top3 = [", ".join(topic[:10]) for _, topic in model.topics.iloc[top3].iterrows()]
+    top3 = distribution.sort_values(ascending=False)[:3]
+    bottom3 = distribution.sort_values(ascending=False)[-3:]
+
+    top3 = [(index, score) for index, score in zip(top3.index, top3)]
+    bottom3 = [(index, score) for index, score in zip(bottom3.index, bottom3)]
+    intruder = random.choice(bottom3)
     top3.append(intruder)
-    return top3, intruder
+    return top3, intruder[1]
