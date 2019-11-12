5232c3fb27e56777eba4992a8f39d28381a0752e
Severin Simmler <severin.simmler@posteo.de>
2019-11-12 16:14:53 +0100

    Delete train.sh

--- train.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-poetry run clustering -- --topics /mnt/data/users/simmler/models/topic-models/gutenberg/gutenberg-10.topics --document-topics /mnt/data/users/simmler/models/topic-models/gutenberg/gutenberg-10.doctopics --stopwords /mnt/data/users/simmler/corpora/gutenberg-stopwords.json --algorithm tsne
-
-poetry run clustering -- --topics /mnt/data/users/simmler/models/topic-models/gutenberg/gutenberg-10.topics --document-topics /mnt/data/users/simmler/models/topic-models/gutenberg/gutenberg-10.doctopics --stopwords /mnt/data/users/simmler/corpora/gutenberg-stopwords.json --algorithm umap
-
-poetry run clustering -- --topics /mnt/data/users/simmler/models/topic-models/pressemitteilungen/pressemitteilungen-20.topics --document-topics /mnt/data/users/simmler/models/topic-models/pressemitteilungen/pressemitteilungen-20.doctopics --stopwords /mnt/data/users/simmler/corpora/pressemitteilungen-stopwords.json --algorithm tsne
-
-poetry run clustering -- --topics /mnt/data/users/simmler/models/topic-models/pressemitteilungen/pressemitteilungen-20.topics --document-topics /mnt/data/users/simmler/models/topic-models/pressemitteilungen/pressemitteilungen-20.doctopics --stopwords /mnt/data/users/simmler/corpora/pressemitteilungen-stopwords.json --algorithm umap
