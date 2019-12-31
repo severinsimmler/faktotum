bda9961625515752ed7f81f7d3d7af1d75f54d5b
severinsimmler <severin.simmler@posteo.de>
2019-12-31 05:41:13 +0100

    fix: imports

--- extract/exploration/__init__.py
+++ extract/exploration/__init__.py
@@ -1,2 +1,3 @@
 from extract.exploration.lda import TopicModel
 from extract.exploration.evaluation import select_topics, select_random_documents
+from extract.exploration.similarities import calculate_document_similarities
--- extract/exploration/similarities.py
+++ extract/exploration/similarities.py
@@ -1,14 +1,30 @@
-from typing import Dict, List
+import random
+from pathlib import Path
+from typing import Dict, List, Union
 
 from flair.embeddings import BertEmbeddings, DocumentPoolEmbeddings, Sentence
 import torch
+import pandas as pd
 import numpy as np
 import sklearn.metrics
 
 
 Dataset = Dict[str, Dict[str, List[str]]]
+Documents = Dict[str, List[str]]
 
 random.seed(23)
+
+
+def calculate_document_similarities(
+    corpus: Union[Path, str], model: Union[Path, str], n: int = 5000
+):
+    embedding = Embedding(model)
+    with Path(corpus).open("r", encoding="utf-8") as file_:
+        dataset = json.load(file_)
+    batch = select_documents(dataset, n)
+    features = pd.DataFrame(embedding.process_batch(batch)).T
+    similarities = sklearn.metrics.pairwise.cosine_similarity(features)
+    return pd.DataFrame(similarities, index=features.index, columns=features.index)
 
 
 def select_documents(dataset: Dataset, n: int = 5000):
@@ -16,10 +32,8 @@ def select_documents(dataset: Dataset, n: int = 5000):
     return {name: data for name, data in dataset.items() if name in selection}
 
 
-
-
 class Embedding:
-    def __init__(self, filepath):
+    def __init__(self, filepath: Union[Path, str]):
         self._bert = BertEmbeddings(filepath)
         self.document_embeddings = DocumentPoolEmbeddings([self._bert])
 
@@ -30,24 +44,11 @@ class Embedding:
             vector = sentence.get_embedding()
             return vector.numpy()
 
-    def process_batch(documents):
+    def process_batch(documents: Documents):
         for name, sentence in self.build_sentences(documents):
             yield name, self.get_vector(sentence)
 
-    def build_sentences(documents):
+    def build_sentences(documents: Documents):
         for name, sentences in documents.items():
-            yield f"{name}-{index}", " ".join(
-                [token["text"] for token in sentence]
-                for index, sentence in sentences.items()
-            )
-
-
-
-
-def foo(filepath, n=5000, model=""):
-    embedding = Embedding(model)
-    with Path(filepath).open("r", encoding="utf-8") as file_:
-        dataset = json.load(file_)
-    batch = select_documents(dataset, n)
-    features = pd.DataFrame(embedding.process_batch(batch)).T
-    similarities = sklearn.metrics.pairwise.cosine_similarity(features)
+            for index, sentence in sentences.items():
+                yield f"{name}-{index}", " ".join(sentence)
