2661e62ed0882426f67fb630433827c2c754c035
severinsimmler <severin.simmler@posteo.de>
2020-01-31 14:06:52 +0100

    feat: crf

--- extract/ner.py
+++ extract/ner.py
@@ -112,6 +112,103 @@ class Baseline:
         print(metric)
         return metric
 
+
+@dataclass
+class Baseline:
+    directory: Union[str, Path]
+    train_file: str
+    test_file: str
+    dev_file: str
+
+    @staticmethod
+    def _parse_data(filepath):
+        sentence = list()
+        for row in Path(filepath).read_text(encoding="utf-8").split("\n"):
+            if row.startswith("# "):
+                continue
+            if row != "":
+                sentence.append(row.split(" "))
+            else:
+                yield sentence
+                sentence = list()
+
+    def from_scratch(self):
+        train_sents = self._parse_data(Path(self.directory, self.train_file))
+        test_sents = self._parse_data(Path(self.directory, self.test_file))
+
+        X_train = [self._sent2features(s) for s in train_sents]
+        y_train = [self._sent2labels(s) for s in train_sents]
+
+        X_test = [self._sent2features(s) for s in test_sents]
+        y_test = [self._sent2labels(s) for s in test_sents]
+
+        crf = sklearn_crfsuite.CRF(
+            algorithm='lbfgs',
+            c1=0.1,
+            c2=0.1,
+            max_iterations=100,
+            all_possible_transitions=True
+        )
+        crf.fit(X_train, y_train)
+
+        y_pred = crf.predict(X_test)
+
+        metric = evaluate_labels("crf-baseline", y_test, y_pred)
+        print(metric)
+        return metric
+
+    def _word2features(sent, i):
+        word = sent[i][0]
+        postag = sent[i][3]
+
+        features = {
+            'bias': 1.0,
+            'word.lower()': word.lower(),
+            'word[-3:]': word[-3:],
+            'word.isupper()': word.isupper(),
+            'word.istitle()': word.istitle(),
+            'word.isdigit()': word.isdigit(),
+            'postag': postag,
+            'postag[:2]': postag[:2],
+        }
+        if i > 0:
+            word1 = sent[i-1][0]
+            postag1 = sent[i-1][1]
+            features.update({
+                '-1:word.lower()': word1.lower(),
+                '-1:word.istitle()': word1.istitle(),
+                '-1:word.isupper()': word1.isupper(),
+                '-1:postag': postag1,
+                '-1:postag[:2]': postag1[:2],
+            })
+        else:
+            features['BOS'] = True
+
+        if i < len(sent)-1:
+            word1 = sent[i+1][0]
+            postag1 = sent[i+1][1]
+            features.update({
+                '+1:word.lower()': word1.lower(),
+                '+1:word.istitle()': word1.istitle(),
+                '+1:word.isupper()': word1.isupper(),
+                '+1:postag': postag1,
+                '+1:postag[:2]': postag1[:2],
+            })
+        else:
+            features['EOS'] = True
+
+        return features
+
+
+    def _sent2features(sent):
+        return [self._word2features(sent, i) for i in range(len(sent))]
+
+    def _sent2labels(sent):
+        return [label for _, label, _, _ in sent]
+
+    def _sent2tokens(sent):
+        return [token for token, _, _, _ in sent]
+
 '''
 first_corpus
     def scratch(self):
