9f86e220dc7ea768389db10a9158b35386d67b8d
severinsimmler <severin.simmler@posteo.de>
2020-02-29 23:26:39 +0100

    fix: typo

--- faktotum/clustering/utils.py
+++ faktotum/clustering/utils.py
@@ -28,7 +28,6 @@ class Embeddings:
         logging.info(f"Loading {path}...")
         self.cbow_w2v = Word2Vec.load(path)
 
-        """
         path = str(Path(model_directory, f"{corpus}-skipgram.word2vec"))
         logging.info(f"Loading {path}...")
         self.skipgram_w2v = Word2Vec.load(path)
@@ -63,7 +62,6 @@ class Embeddings:
             path = str(Path(model_directory, "ner-smartdata"))
         logging.info(f"Loading {path}...")
         self.bert_ner = BertEmbeddings(path)
-        """
 
     def vectorize(self, sentences, model, add_adj=False, add_nn=False, add_per=False):
         X = list()
