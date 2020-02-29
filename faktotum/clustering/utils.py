2b7c1920db95a8c96dea163c3daf5e2b15247d08
severinsimmler <severin.simmler@posteo.de>
2020-03-01 00:28:46 +0100

    fix: typo

--- faktotum/clustering/utils.py
+++ faktotum/clustering/utils.py
@@ -86,7 +86,7 @@ class Embeddings:
         self, sentence, token_indices, model, add_adj=False, add_nn=False, add_per=False
     ):
         self._add_tokens(token_indices, add_adj, add_nn, add_per)
-        tokens = [token for i, token in enumerate(sentence) if i in token_indices]
+        tokens = [token[0] for i, token in enumerate(sentence) if i in token_indices]
         return sum(self._get_classic_embedding(tokens, model)) / len(tokens)
 
     def _bert_vectorization(
@@ -96,7 +96,7 @@ class Embeddings:
         sentence_ = Sentence(text, use_tokenizer=False)
         model.embed(sentence_)
         self._add_tokens(token_indices, add_adj, add_nn, add_per)
-        tokens = [token for i, token in enumerate(sentence_) if i in token_indices]
+        tokens = [token[0] for i, token in enumerate(sentence_) if i in token_indices]
         return sum(self._get_bert_embedding(tokens)) / len(tokens)
 
     @staticmethod
