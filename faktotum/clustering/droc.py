c2b1dbe5b7d1ed92ba35fedfaf40af6c136968be
severinsimmler <severin.simmler@posteo.de>
2020-03-01 01:34:40 +0100

    fix: typo

--- faktotum/clustering/droc.py
+++ faktotum/clustering/droc.py
@@ -1,9 +1,10 @@
-from faktotum.clustering.utils import Embeddings, Clustering
-from pathlib import Path
 import json
-import pandas as pd
 import logging
+from pathlib import Path
 
+import pandas as pd
+
+from faktotum.clustering.utils import Clustering, Embeddings
 
 logging.basicConfig(format="%(message)s", level=logging.INFO)
 
@@ -19,6 +20,53 @@ def load_data():
 
 
 def compare_embeddings(model_directory):
+    stats = list()
+    index = list()
+
+    data = load_data()
+    embeddings = Embeddings(model_directory, "gutenberg")
+
+    for embedding, model in {
+        ("CBOW_{w2v}", embeddings.cbow_w2v),
+        ("Skipgram_{w2v}", embeddings.skipgram_w2v),
+        ("Facebook CBOW_{ft}", embeddings.cbow_ft_fb),
+        ("CBOW_{ft}", embeddings.cbow_ft),
+        ("Skipgram_{ft}", embeddings.skipgram_ft),
+        ("dBERT", embeddings.bert_g),
+        ("dBERT_{\ddagger}", embeddings.bert_ga),
+        ("mBERT", embeddings.bert_m),
+        ("mBERT_{\ddagger}", embeddings.bert_ma),
+        ("BERT_{ner}", embeddings.bert_ner),
+    }:
+        for suffix, features in {
+            ("", {"add_adj": False, "add_nn": False, "add_per": False}),
+            (" + ADJ", {"add_adj": True, "add_nn": False, "add_per": False}),
+            (" + NN", {"add_adj": False, "add_nn": True, "add_per": False}),
+            (" + PER", {"add_adj": False, "add_nn": False, "add_per": True}),
+            (" + ADJ + NN + PER", {"add_adj": True, "add_nn": True, "add_per": True}),
+        }:
+            approach = embedding + suffix
+            logging.info(approach)
+            scores = list()
+            for novel in data.values():
+                X, y = embeddings.vectorize(novel, model, **features)
+                clustering = Clustering("kmeans", X, y)
+                score = clustering.evaluate()
+                scores.append(score)
+            scores = pd.DataFrame(scores)
+            values = {
+                index: f"{mean} (±{std})"
+                for index, mean, std in zip(
+                    scores.columns, scores.mean().round(2), scores.std().round(2)
+                )
+            }
+            logging.info(json.dumps(values))
+            stats.append(values)
+            index.append(approach)
+    return pd.DataFrame(stats, index=index)
+
+
+def compare_algorithms(model_directory):
     stats = list()
     index = list()
 
