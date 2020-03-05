c348cf4de79d4d93c07d7a79208b89bb934542ce
severinsimmler <severin.simmler@posteo.de>
2020-03-05 15:01:39 +0100

    fix: fn

--- faktotum/linking/droc.py
+++ faktotum/linking/droc.py
@@ -18,21 +18,16 @@ EMBEDDING = BertEmbeddings("/mnt/data/users/simmler/model-zoo/ner-droc")
 
 
 class EntityLinker:
-    def __init__(self, dataset=None):
-        if not dataset:
-            self.pre_tagged = False
-            module_folder = Path(__file__).resolve().parent.parent
-            self.corpus_folder = Path(module_folder, "data", "droc", "linking")
-            self.train = self._load_corpus("train")
-            self.test = self._load_corpus("test")
-            self.dev = self._load_corpus("dev")
-            self.dataset = dict()
-            for i, data in enumerate([self.test, self.dev, self.train]):
-                for key, value in data.items():
-                    self.dataset[f"{i}_{key}"] = value
-        else:
-            self.pre_tagged = True
-            self.dataset = dataset
+    def __init__(self):
+        module_folder = Path(__file__).resolve().parent.parent
+        self.corpus_folder = Path(module_folder, "data", "droc", "linking")
+        self.train = self._load_corpus("train")
+        self.test = self._load_corpus("test")
+        self.dev = self._load_corpus("dev")
+        self.dataset = dict()
+        for i, data in enumerate([self.test, self.dev, self.train]):
+            for key, value in data.items():
+                self.dataset[f"{i}_{key}"] = value
 
     def _load_corpus(self, dataset: str):
         textfile = Path(self.corpus_folder, f"{dataset}.txt")
@@ -62,42 +57,35 @@ class EntityLinker:
 
     @staticmethod
     def _vectorize(sentence, index=None, mask_entity: bool = False):
-        if isinstance(sentence[0][0], float):
-            for entity, indices in index.items():
-                vector = sentence[indices[0]]
-                for i in indices[1:]:
-                    vector = vector + sentence[i]
-                yield vector / len(indices)
-        else:
-            if not index:
-                tokens = list()
-                index = defaultdict(list)
-                for i, token in enumerate(sentence):
-                    if token[2] == "-":
-                        tokens.append(token[0])
-                    else:
-                        if index[token[2]]:
-                            if index[token[2]][-1] + 1 == i:
-                                index[token[2]].append(i)
-                            else:
-                                print("ERROR: Same entity multiple times.")
-                        else:
+        if not index:
+            tokens = list()
+            index = defaultdict(list)
+            for i, token in enumerate(sentence):
+                if token[2] == "-":
+                    tokens.append(token[0])
+                else:
+                    if index[token[2]]:
+                        if index[token[2]][-1] + 1 == i:
                             index[token[2]].append(i)
-                        if mask_entity:
-                            tokens.append("[MASK]")
                         else:
-                            tokens.append(token[0])
-                text = " ".join(tokens)
-            else:
-                tokens = list()
-                for i, token in enumerate(sentence):
-                    if i in list(index.values()) and mask_entity:
+                            print("ERROR: Same entity multiple times.")
+                    else:
+                        index[token[2]].append(i)
+                    if mask_entity:
                         tokens.append("[MASK]")
                     else:
                         tokens.append(token[0])
-                text = " ".join(tokens)
-            sentence = Sentence(text, use_tokenizer=False)
-            EMBEDDING.embed(sentence)
+            text = " ".join(tokens)
+        else:
+            tokens = list()
+            for i, token in enumerate(sentence):
+                if i in list(index.values()) and mask_entity:
+                    tokens.append("[MASK]")
+                else:
+                    tokens.append(token[0])
+            text = " ".join(tokens)
+        sentence = Sentence(text, use_tokenizer=False)
+        EMBEDDING.embed(sentence)
         for entity, indices in index.items():
             vector = sentence[indices[0]].get_embedding().numpy()
             for i in indices[1:]:
@@ -110,111 +98,60 @@ class EntityLinker:
             tp = 0
             fp = 0
             fn = 0
-            if not self.pre_tagged:
-                kb = self._build_knowledge_base(novel)
-                for sentence in novel:
-                    mentions = [token for token in sentence if token[2] != "-"]
-                    mention_vectors = [
-                        list(self._vectorize(sentence, {token[2]: [i]}))
-                        for i, token in enumerate(sentence)
-                        if token[2] != "-"
-                    ]
-                    for mention, mention_vector in zip(mentions, mention_vectors):
-                        matches = defaultdict(list)
-                        for values in kb.values():
-                            if len(values["CONTEXT"]) == 1:
-                                skip = True
-                                continue
-                            skip = False
-                            valid_sentences = list()
-                            for context in values["CONTEXT"]:
-                                # Filter the current sentence
-                                if context != sentence:
-                                    valid_sentences.append(context)
-                            for context in valid_sentences:
-                                for i, token in enumerate(context):
-                                    if token[2] != "-" and token[0] == mention[0]:
-                                        vector = list(
-                                                self._vectorize(
-                                                    context,
-                                                    index={token[2]: [i]},
-                                                    mask_entity=mask_entity,
-                                                )
-                                            )
-                                        matches[token[2]].append(vector)
-                        if not skip:
-                            if len(matches) == 0:
-                                fn += 1
-                            elif len(matches) == 1:
-                                if list(matches)[0] == mention[2]:
-                                    tp += 1
-                                else:
-                                    fp += 1
-                            else:
-                                max_score = 0.0
-                                candidate = None
-                                for identifier, vector in matches.items():
-                                    score = cosine_similarity(mention_vector[0], vector[0][0])[0][0]
-                                    if score > max_score:
-                                        max_score = score
-                                        candidate = identifier
-                                if candidate == mention[2]:
-                                    tp += 1
-                                else:
-                                    fp += 1
-                try:
-                    precision = self.precision(tp, fp)
-                    recall = self.recall(tp, fn)
-                    f1 = self.f1(precision, recall)
-                    stats.append(
-                        {"precision": precision, "recall": recall, "f1": f1,}
-                    )
-                except ZeroDivisionError:
-                    pass
-            return pd.DataFrame(stats).describe()
-        else:
+            kb = self._build_knowledge_base(novel)
             for sentence in novel:
-                entities = defauldict(list)
-                for e in sentence["entities"]:
-                    entities[list(e)[0]].append(list(e.values())[0])
-                for entity, indices in entities.items():
-                    max_score = 0.0
-                    candidate = None
-                    if mask_entity:
-                        vector = np.array(sentence["masked_embedding"][indices[0]])
-                        for index in indices[1:]:
-                            vector = vector + np.array(sentence["masked_embedding"][index])
-                    else:
-                        vector = np.array(sentence["embedding"][indices[0]])
-                        for index in indices[1:]:
-                            vector = vector + np.array(sentence["embedding"][index])
-                    vector = vector / len(indices)
+                mentions = [token for token in sentence if token[2] != "-"]
+                indices = defaultdict(list)
+                for i, token in enumerate(sentence):
+                    if token[2] != "-":
+                        indices[token[2]].append(i)
+                mention_vectors = list(self._vectorize(sentence, indices))
+                return mention_vectors
 
-                    for other_sentence in novel:
-                        if sentence != other_sentence:
-                            compare = defauldict(list)
-                            for e in other_sentence["entities"]:
-                                compare[list(e)[0]].append(list(e.values())[0])
-                            for key, values in compare.items():
-                                if mask_entity:
-                                    other_vector = np.array(sentence["masked_embedding"][values[0]])
-                                    for index in values[1:]:
-                                        other_vector = other_vector + np.array(sentence["masked_embedding"][index])
-                                else:
-                                    other_vector = np.array(sentence["embedding"][values[0]])
-                                    for index in values[1:]:
-                                        other_vector = other_vector + np.array(sentence["embedding"][index])
-                                other_vector = other_vector / len(values)
-                                score = cosine_similarity(vector, other_vector)
-                                if max_score < score:
+
+                for mention, mention_vector in zip(mentions, mention_vectors):
+                    matches = defaultdict(list)
+                    for values in kb.values():
+                        if len(values["CONTEXT"]) == 1:
+                            skip = True
+                            continue
+                        skip = False
+                        valid_sentences = list()
+                        for context in values["CONTEXT"]:
+                            # Filter the current sentence
+                            if context != sentence:
+                                valid_sentences.append(context)
+                        for context in valid_sentences:
+                            for i, token in enumerate(context):
+                                if token[2] != "-" and token[0] == mention[0]:
+                                    vector = list(
+                                            self._vectorize(
+                                                context,
+                                                index={token[2]: [i]},
+                                                mask_entity=mask_entity,
+                                            )
+                                        )
+                                    matches[token[2]].append(vector)
+                    if not skip:
+                        if len(matches) == 0:
+                            fn += 1
+                        elif len(matches) == 1:
+                            if list(matches)[0] == mention[2]:
+                                tp += 1
+                            else:
+                                fp += 1
+                        else:
+                            max_score = 0.0
+                            candidate = None
+                            for identifier, vector in matches.items():
+                                score = cosine_similarity(mention_vector[0], vector[0][0])[0][0]
+                                if score > max_score:
                                     max_score = score
-                                    candidate = key
-                    if entity == candidate:
-                        print("true", max_score)
-                        tp += 1
-                    else:
-                        print("false", max_score)
-                        fp += 1
+                                    candidate = identifier
+                            if candidate == mention[2]:
+                                tp += 1
+                            else:
+                                fp += 1
             try:
                 precision = self.precision(tp, fp)
                 recall = self.recall(tp, fn)
