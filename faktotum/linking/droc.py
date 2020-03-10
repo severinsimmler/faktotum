e1573fb4e2bd2f73b379f41b596e46ee82d1ca10
severinsimmler <severin.simmler@posteo.de>
2020-03-10 11:11:42 +0100

    fix: dependencies

--- faktotum/linking/droc.py
+++ faktotum/linking/droc.py
@@ -18,7 +18,7 @@ from faktotum.regression import Regression
 import random
 
 
-EMBEDDING = BertEmbeddings("/mnt/data/users/simmler/model-zoo/ner-droc")
+EMBEDDING = BertEmbeddings("/mnt/data/users/simmler/model-zoo/entity-embeddings-droc")
 
 
 class EntityLinker:
