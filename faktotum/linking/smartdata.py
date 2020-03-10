befb32f781efecd472a88017d1b34eca2489b345
severinsimmler <severin.simmler@posteo.de>
2020-03-10 20:25:39 +0100

    fix: typo

--- faktotum/linking/smartdata.py
+++ faktotum/linking/smartdata.py
@@ -24,7 +24,7 @@ EMBEDDING = BertEmbeddings(
 
 
 class EntityLinker:
-    _string_similarity_threshold = 0.478836
+    _string_similarity_threshold = 0.689922
 
     def __init__(self, kb_dir: str):
         module_folder = Path(__file__).resolve().parent.parent
