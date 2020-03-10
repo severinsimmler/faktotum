ca9c30da67e8c62b6a3bb82eda5c169d1fbe98c9
severinsimmler <severin.simmler@posteo.de>
2020-03-10 21:50:18 +0100

    fix: typo

--- faktotum/clustering/smartdata.py
+++ faktotum/clustering/smartdata.py
@@ -44,8 +44,8 @@ def ward(model_directory):
     index = list()
 
     data = load_data(all_=False)
-    embeddings = Embeddings(model_directory, "presse", load="entity")
-    X, y = embeddings.vectorize(data, embeddings.entity_bert)
+    embeddings = Embeddings(model_directory, "presse", load="all-masked")
+    X, y = embeddings.vectorize(data, embeddings.entity_all_bert)
     clustering = Clustering("ward", X, y)
     score = clustering.evaluate()
     return score
