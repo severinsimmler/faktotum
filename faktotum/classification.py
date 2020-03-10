d154e26e684f56ee95002969d7b0996fae890803
severinsimmler <severin.simmler@posteo.de>
2020-03-10 10:53:15 +0100

    fix: dependencies

--- faktotum/classification.py
+++ faktotum/classification.py
@@ -1,14 +1,29 @@
 class Classifier:
     @staticmethod
-    def _build_model(self, input_shape):
+    def _build_model(self, input_shape, conv1d=False):
         model = models.Sequential()
-        model.add(layers.Dense(64, activation="relu", input_shape=(input_shape,)))
-        model.add(layers.Dense(64, activation="relu"))
-        model.add(layers.Dense(1))
+        if conv1d:
+            sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
+            embedded_sequences = embedding_layer(sequence_input)
+            x = Conv1D(128, 5, activation='relu')(embedded_sequences)
+            x = MaxPooling1D(5)(x)
+            x = Conv1D(128, 5, activation='relu')(x)
+            x = MaxPooling1D(5)(x)
+            x = Conv1D(128, 5, activation='relu')(x)
+            x = GlobalMaxPooling1D()(x)
+            x = Dense(128, activation='relu')(x)
+            preds = Dense(len(labels_index), activation='softmax')(x)
 
-        model.compile(optimizer="rmsprop", loss="mse", metrics=["mae"])
+        else:
+            model.add(layers.Dense(64, activation="relu", input_shape=(input_shape,)))
+            model.add(layers.Dense(64, activation="relu"))
+            model.add(layers.Dense(1))
+
+        model.compile(loss='categorical_crossentropy',
+                optimizer='rmsprop',
+                metrics=['acc'])
         return model
 
-    def fit(self, X_train, y_train, epochs=100):
-        model = self._build_model(self.X_train.shape[1])
+    def fit(self, X_train, y_train, epochs=100, conv1d=False):
+        model = self._build_model(self.X_train.shape[1], conv1d)
         return model.fit(X_train, y_train, epochs=epochs, batch_size=64, verbose=1)
