3d48fd72d7239ad02c09113cfbe91a0a65c61e8d
severinsimmler <severin.simmler@posteo.de>
2020-03-11 20:44:15 +0100

    fix: type

--- faktotum/linking/smartdata.py
+++ faktotum/linking/smartdata.py
@@ -21,7 +21,7 @@ from strsimpy.jaro_winkler import JaroWinkler
 
 JARO_WINKLER = JaroWinkler()
 EMBEDDING = BertEmbeddings(
-    "/mnt/data/users/simmler/model-zoo/entity-embeddings-smartdata"
+    "/mnt/data/users/simmler/language-models/presse/multi"
 )
 
 
@@ -238,7 +238,7 @@ class EntityLinker:
             "accuracy": self.accuracy(tp, fp),
             "precision": self.precision(tp, fp),
             "num_candidates": statistics.mean(num_candidates),
-            "embedding": "entity-embeddings-smartdata"
+            "embedding": "language-models/presse/multi"
         }
 
     @staticmethod
