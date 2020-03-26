286cf9e5a931ac682caaa607bac0c1bcafe07130
severinsimmler <severin.simmler@posteo.de>
2020-03-26 04:31:23 +0100

    similarity

--- faktotum/similarity.py
+++ faktotum/similarity.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import torch
 
 import flair
+from flair.data import DataPair, DataPoint
 from flair.datasets import FlairDataset
 from flair.embeddings import DocumentRNNEmbeddings, BertEmbeddings
 from flair.models.similarity_learning_model import SimilarityLearner, CosineSimilarity, RankingLoss
