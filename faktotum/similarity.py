287b082edc41583211a9dd5a11ab8580bf529ab9
severinsimmler <severin.simmler@posteo.de>
2020-03-26 16:06:48 +0100

    smartadata

--- faktotum/similarity.py
+++ faktotum/similarity.py
@@ -58,7 +58,7 @@ class FaktotumDataset(FlairDataset):
     @staticmethod
     def _load_corpus(name):
         module = Path(__file__).resolve().parent
-        data = Path(module, "data", "droc", "similarity", f"{name}.json").read_text(encoding="utf-8")
+        data = Path(module, "data", "smartdata", "similarity", f"{name}.json").read_text(encoding="utf-8")
         return json.loads(data)
 
     def __len__(self):
@@ -158,7 +158,7 @@ class EntitySimilarityLearner(SimilarityLearner):
 
 def test():
     corpus = FaktotumDataset()
-    embedding = BertEmbeddings('/mnt/data/users/simmler/model-zoo/ner-droc')
+    embedding = BertEmbeddings('/mnt/data/users/simmler/model-zoo/bert-multi-presse-adapted')
 
     source_embedding = embedding
     target_embedding = embedding
@@ -175,8 +175,8 @@ def test():
     trainer: ModelTrainer = ModelTrainer(similarity_model,corpus, optimizer=torch.optim.SGD)
 
     trainer.train(
-        'droc-cosine-bcp-improved-loss',
-        mini_batch_size=32,
+        'smartdata-cosine-bcp-improved-loss',
+        mini_batch_size=16,
         embeddings_storage_mode='none'
     )
 
