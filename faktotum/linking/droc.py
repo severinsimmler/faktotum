2415bc66bc44ad7b8b8d02ce7515683f5af7d984
severinsimmler <severin.simmler@posteo.de>
2020-03-26 02:51:49 +0100

    fix

--- faktotum/linking/droc.py
+++ faktotum/linking/droc.py
@@ -166,29 +166,22 @@ class EntityLinker:
 
                         if best_candidate == identifier:
                             tp += 1
-                            print({"true": name,
-                                        "pred": best_mention,
-                                        "true_id": identifier,
-                                        "pred_id": best_candidate,
-                                        "score": max_score,
-                                        "sentence": " ".join([token[0] for token in sentence]),
-                                        "context": best_sent})
                             tps.append({"true": name,
                                         "pred": best_mention,
                                         "true_id": identifier,
                                         "pred_id": best_candidate,
-                                        "score": max_score,
+                                        "score": max_score[0][0],
                                         "sentence": " ".join([token[0] for token in sentence]),
-                                        "context": best_sent})
+                                        "context": " ".join([token[0] for token in best_sent])})
                         else:
                             fp += 1
                             fps.append({"true": name,
                                         "pred": best_mention,
                                         "true_id": identifier,
                                         "pred_id": best_candidate,
-                                        "score": max_score,
+                                        "score": max_score[0][0],
                                         "sentence": " ".join([token[0] for token in sentence]),
-                                        "context": best_sent})
+                                        "context": " ".join([token[0] for token in best_sent])})
             with open(f"droc-{i}.json", "w", encoding="utf-8") as f:
                 json.dump({"tps": tps, "fps": fps}, f, ensure_ascii=False, indent=4)
             stats.append(
