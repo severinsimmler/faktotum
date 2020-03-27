a8c1b8581de4bfb71a81770180ea7620f07b23aa
severinsimmler <severin.simmler@posteo.de>
2020-03-27 23:08:51 +0100

    fix

--- faktotum/similarity.py
+++ faktotum/similarity.py
@@ -187,7 +187,7 @@ def test():
 
     trainer.train(
         "droc-similarity-model",
-        mini_batch_size=32,
+        mini_batch_size=128,
         max_epochs=10,
         embeddings_storage_mode="none",
     )
