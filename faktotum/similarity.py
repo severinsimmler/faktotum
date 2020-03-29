82e88ec98c6adaa40c4c04db4f35af0ed55e5c4e
severinsimmler <severin.simmler@posteo.de>
2020-03-29 03:03:22 +0200

    fix

--- faktotum/similarity.py
+++ faktotum/similarity.py
@@ -3,6 +3,7 @@ import json
 from pathlib import Path
 
 import flair
+import pandas as pd
 import torch
 import tqdm
 from flair.data import Sentence, DataPair
@@ -100,6 +101,50 @@ class SentenceSimilarity(SimilarityLearner):
 
         return loss
 
+    def evaluate(
+        self,
+        data_loader: DataLoader,
+        out_path: Path = None,
+        embedding_storage_mode="none",
+    ) -> (Result, float):
+        ranks = list()
+        with torch.no_grad():
+            targets = list()
+            targets_y = list()
+            for data_points in data_loader:
+                targets.extend(self._embed_target(data_points).to(self.eval_device))
+                targets_y.extend([sentence.second.person for sentence in data_points])
+                store_embeddings(data_points, embedding_storage_mode)
+
+            for data_points in data_loader:
+                sources = self._embed_source(data_points).to(self.eval_device)
+                sources_y = [sentence.first.person for sentence in data_points]
+
+                scores = list()
+                agreement = list()
+                for source, source_y in zip(sources, sources_y):
+                    for target, target_y in zip(targets, targets_y):
+                        score = self.similarity_measure(source, target).item()
+                        scores.append(score)
+                        agreement.append(source_y == target_y)
+                
+                df = pd.DataFrame({"scores": scores, "agreement": agreement})
+                df = df.sort_values("scores", ascending=False).reset_index(drop=True)
+                df = df[df["agreement"] == True]
+                rank = min(df.index)
+                ranks.append(1 - (rank / len(targets_y)))
+        
+        results_header_str = "\t".join(["Median rank", "Mean rank", "Standard deviation"])
+        epoch_results_str = "\t".join([str(np.median(ranks)), str(np.mean(ranks)), str(np.std(ranks))])
+        return (
+            Result(
+                np.mean(ranks),
+                results_header_str,
+                epoch_results_str,
+                "",
+            ),
+            0,
+        )
 
 def train(corpus_name="droc", embeddings_path="/mnt/data/users/simmler/model-zoo/ner-droc"):
     corpus = FaktotumDataset(corpus_name)
