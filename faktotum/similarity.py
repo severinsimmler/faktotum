cf939bde0ce1f80590000ec0c0842985b9b28732
severinsimmler <severin.simmler@posteo.de>
2020-03-30 23:08:36 +0200

    fix

--- faktotum/similarity.py
+++ faktotum/similarity.py
@@ -44,7 +44,7 @@ class FaktotumDataset(FlairDataset):
         self.dev = list()
         self.test = list()
 
-        for instance in tqdm.tqdm(self._load_corpus("train")):
+        for instance in tqdm.tqdm(self._load_corpus("test")):
             sentence = Sentence(instance["sentence"], use_tokenizer=False)
             context = Sentence(instance["context"], use_tokenizer=False)
             sentence.person = instance["person"]
@@ -54,7 +54,7 @@ class FaktotumDataset(FlairDataset):
             point = DataPair(sentence, context)
             self.train.append(point)
 
-        for instance in tqdm.tqdm(self._load_corpus("test")):
+        for instance in tqdm.tqdm(self._load_corpus("dev")):
             sentence = Sentence(instance["sentence"], use_tokenizer=False)
             context = Sentence(instance["context"], use_tokenizer=False)
             sentence.person = instance["person"]
