f07f21957b10db7fcedaa4998074bf3b0d3b6eed
severinsimmler <severin.simmler@posteo.de>
2020-04-06 06:56:40 +0200

    cleanup

--- faktotum/__init__.py
+++ faktotum/__init__.py
@@ -1,3 +1,7 @@
+import logging
+import transformers
 from faktotum import typing, utils
 from faktotum.kb import KnowledgeBase
 from faktotum.pipelines import ned, nel, ner
+
+logging.getLogger("transformers").setLevel(logging.ERROR)
