7ab91f7057e60eebbc100dc5f83f8dea2cad6d72
severinsimmler <severin.simmler@posteo.de>
2020-04-10 13:28:33 +0200

    fix

--- faktotum/pipelines.py
+++ faktotum/pipelines.py
@@ -96,16 +96,12 @@ def _get_best_candidate(mention, mention_embedding, kb, similarity_threshold):
     best_candidate = "NIL"
     best_score = 0.0
     logging.info("Searching in knowledge base for candidates...")
-    pbar = None
-    for identifier, values in kb.items():
+    for identifier, values in tqdm.tqdm(kb.items()):
         for i, (index, context, candidate_embedding) in enumerate(
             zip(values["ENTITY_INDICES"], values["CONTEXTS"], values["EMBEDDINGS"])
         ):
             candidate = " ".join(context[i] for i in index)
-            if (
-                mention.lower() in candidate.lower()
-                or JARO_WINKLER.similarity(mention, candidate) >= similarity_threshold
-            ):
+            if JARO_WINKLER.similarity(mention, candidate) >= similarity_threshold:
                 if not candidate_embedding:
                     candidate_embedding = _vectorize_context(
                         kb.pipeline, context, index
@@ -115,10 +111,6 @@ def _get_best_candidate(mention, mention_embedding, kb, similarity_threshold):
                 if score > best_score:
                     best_score = score
                     best_candidate = identifier
-        if not pbar:
-            pbar = tqdm.tqdm(total=len(kb))
-        pbar.update()
-    pbar.close()
     return best_candidate, best_score
 
 
